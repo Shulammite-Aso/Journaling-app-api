8848ba9055e39a8d08cd257efefd4f62bf9508da
Shulammite-Aso <asoamarachi@gmail.com>
2021-10-01 22:17:56 +0100

    add after hook to test

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { handleUserSignup } from "../controller/user.controller";
 import { handleCreateUserSession, invalidateUserSessionHandler } from "../controller/userSession.controller";
+import User from "../model/user.model"
 import validate from "../middleware/validate";
 import requiresUser from "../middleware/requireUser";
 import { createUserSchema, createUserSessionSchema } from "../schema/userSchema";
@@ -16,6 +17,17 @@ const router = express.Router();
 
     // LOGOUT ENDPOINT
     router.delete("/api/sessions", requiresUser, invalidateUserSessionHandler);
+
+    // Currently this route is solely for the purpose of cleaning up after a test
+    router.post("/test", function(req, res) {
+         User.deleteOne({ name: req.body.name }, function (err: any) {
+            if(err) console.log(err);
+            console.log("Successful deletion");
+            return res.sendStatus(200);
+          });
+    });
+
+    // DEVELOPMENT BRANCH NEXT
 
 // USER ENDPOINT
 
--- test/userAuthentication.test.ts
+++ test/userAuthentication.test.ts
@@ -4,7 +4,6 @@ import config from "config";
 import express, { Request, Response } from "express";
 import chaiHttp = require('chai-http')
 import { request, expect } from 'chai';
-import { app } from "../src/app"
 import User from "../src/model/user.model"
 
 chai.use(chaiHttp);
@@ -13,6 +12,13 @@ const host = config.get("host") as string;
 const port = config.get("port") as string;
 
 describe('user authentication', () => {
+
+    // beforeEach(function() {
+    //     return User.remove({'name':'test'});
+    //   });
+
+    
+
     describe('/ POST signup', () => {
         it('it should register a user', async () => {
             let user = {
@@ -27,6 +33,7 @@ describe('user authentication', () => {
     });
 
     describe('/ POST sessions', () => {
+
         it('it should create a user session and return access and refresh token', async () => {
             let user = {
                 email: "test@gmail.com",
@@ -38,8 +45,20 @@ describe('user authentication', () => {
             expect(res.body).to.have.property('accessToken');
             expect(res.body).to.have.property('refreshToken');
         });
+
+        
     });
 
+    
 });
+
+after(async function() {
+    let user = {
+        email: "test@gmail.com",
+        name: "test",
+        password: "test123"
+    }
+    const res = await request(host+":"+port).post('/api/v1/test').send(user);
+  });
 
 ///
